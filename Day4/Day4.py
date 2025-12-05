92ef6eae8c09121eb8da9c7e322eacc23b305f3a
Bassam <basmaym148@gmail.com>
2025-12-05 07:19:00 -0500

    Add Day 4

---
+++ Day4/Day4.py
@@ -0,0 +1,76 @@
+raw_input = open("./input.txt", "r").read().split("\n")
+
+grid = [list("."*(len(raw_input[0]) + 2)),
+        *[list(f".{x}.") for x in raw_input],
+        list("."*(len(raw_input[0]) + 2))]
+
+total_accessible = 0
+
+##### Part 1
+# for y in range(1, len(grid) - 1):
+#     for x in range(1, len(grid[0]) - 1):
+#         if grid[y][x] == ".":
+#             continue
+
+#         adj = 0
+
+#         if grid[y - 1][x - 1] == "@":
+#             adj += 1
+#         if grid[y - 1][x] == "@":
+#             adj += 1
+#         if grid[y - 1][x + 1] == "@":
+#             adj += 1
+
+#         if grid[y][x - 1] == "@":
+#             adj += 1
+#         if grid[y][x + 1] == "@":
+#             adj += 1
+
+#         if grid[y + 1][x - 1] == "@":
+#             adj += 1
+#         if grid[y + 1][x] == "@":
+#             adj += 1
+#         if grid[y + 1][x + 1] == "@":
+#             adj += 1
+
+#         if adj < 4:
+#             total_accessible += 1
+
+##### Part 2
+# halt = False
+# while not halt:
+#     halt = True
+
+#     for y in range(1, len(grid) - 1):
+#         for x in range(1, len(grid[0]) - 1):
+#             if grid[y][x] == ".":
+#                 continue
+
+#             adj = 0
+
+#             if grid[y - 1][x - 1] == "@":
+#                 adj += 1
+#             if grid[y - 1][x] == "@":
+#                 adj += 1
+#             if grid[y - 1][x + 1] == "@":
+#                 adj += 1
+
+#             if grid[y][x - 1] == "@":
+#                 adj += 1
+#             if grid[y][x + 1] == "@":
+#                 adj += 1
+
+#             if grid[y + 1][x - 1] == "@":
+#                 adj += 1
+#             if grid[y + 1][x] == "@":
+#                 adj += 1
+#             if grid[y + 1][x + 1] == "@":
+#                 adj += 1
+
+#             if adj < 4:
+#                 total_accessible += 1
+#                 grid[y][x] = "."
+#                 halt = False
+
+
+# print(total_accessible)
